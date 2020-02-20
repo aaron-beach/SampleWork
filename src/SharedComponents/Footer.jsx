bddfdb78526d0aaec41b488e21e99b74650e4ec5
aaron_beach <aaron.r.beach@gmail.com>
2020-02-20 15:04:04 -0500

    remove spa

---
+++ src/SharedComponents/Footer.jsx
@@ -0,0 +1,60 @@
+import { Container } from "react-bootstrap";
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+function Footer() {
+  const { t } = useTranslation();
+  
+  return (
+    <footer className="footer_area f_bg pt_60">
+      <div className="footer_bottom">
+        <Container>
+          <div className="row align-items-center">
+            <div className="col-lg-4 col-md-5 col-sm-6">
+              <p className="mb-0 f_400">
+                Copyright © 2020 <a href="/">Sample Inc.</a>
+              </p>
+            </div>
+            <div className="col-lg-4 col-md-3 col-sm-6">
+              <div className="f_social_icon_two text-center">
+                <a href="#">
+                  <i className="fab fa-medium-m"></i>
+                </a>
+                <a href="#">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a href="#">
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a href="#">
+                  <i className="fab fa-instagram"></i>
+                </a>
+                <a href="#">
+                  <i className="fab fa-telegram-plane"></i>
+                </a>
+                <a href="#">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-4 col-sm-12">
+              <ul className="list-unstyled f_menu text-right">
+                <li>
+                  <a href="/terms">{t("footer.terms")}</a>
+                </li>
+                <li>
+                  <a href="/cookies">{t("footer.cookies")}</a>
+                </li>
+                <li>
+                  <a href="/privacy">{t("footer.privacy")}</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
